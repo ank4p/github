9c7821dacf555511c52e73cb974b0cc30afa4c87
antek <antoni.knapczyk@outlook.com>
2025-06-07 16:50:03 +0200

    index.html

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({ plugins: [tailwindcss(),], })
+
+const repsonse = fetch("https://nwiszrnyyocnkvzgyrsl.supabase.co/rest/v1/article?select=*", {
+    headers
+};
